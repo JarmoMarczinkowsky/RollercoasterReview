98c64ba5810f201c83bd3024ea91735f70932c48
JarmoMarczinkowsky <d278529@edu.rocwb.nl>
2023-03-21 10:52:57 +0100

    Added project

---
+++ RollercoasterReview/RollercoasterReview/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RollercoasterReview.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
